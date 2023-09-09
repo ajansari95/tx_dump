9f74d76ebe0588008e33488fb046cdbb6b89edef
ajansari95 <ajansari9585@gmail.com>
2023-09-09 08:37:45 +0530

    models added

---
+++ src/models/message.rs
@@ -0,0 +1,51 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Amount {
+    pub denom: String,
+    pub amount: String,
+}
+
+/// Represents various message types from the CosmosSDK.
+/// The `Message` enum is used to deserialize different message types based on the `@type` field
+/// in the JSON data. Each variant of the enum corresponds to a specific message type.
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "@type")]
+pub enum Message {
+    #[serde(rename = "/cosmos.bank.v1beta1.MsgSend")]
+    MsgSend {
+        from_address: String,
+        to_address: String,
+        amount: Vec<Amount>,
+    },
+
+    #[serde(rename = "/cosmos.staking.v1beta1.MsgDelegate")]
+    MsgDelegate {
+        delegator_address: String,
+        validator_address: String,
+        amount: Amount,
+    },
+
+    #[serde(rename = "/ibc.applications.transfer.v1.MsgTransfer")]
+    MsgTransfer {
+        source_port: String,
+        source_channel: String,
+        token: Amount,
+        sender: String,
+        receiver: String,
+        timeout_height: TimeoutHeight,
+        timeout_timestamp: String,
+        memo: String,
+    },
+
+    /// A fallback variant for any unexpected message types.
+    #[serde(other)]
+    Other,
+
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TimeoutHeight {
+    revision_number: String,
+    revision_height: String,
+}
---
+++ src/models/transaction.rs
@@ -0,0 +1,40 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use super::message::Message;
+
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ResponseData {
+    txs: Vec<Tx>,
+    tx_responses: Vec<TxResponse>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Tx {
+    body: Body,
+    auth_info: Value,
+    signatures: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Body {
+    messages: Vec<Message>,
+    memo: String,
+    timeout_height: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TxResponse {
+    height: String,
+    txhash: String,
+    codespace: String,
+    code: i32,
+    data: String,
+    raw_log: Value,
+    logs: Value,
+    gas_wanted: String,
+    gas_used: String,
+    timestamp: String,
+}
+
